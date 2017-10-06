b1f96ee1db45987559f4f8ce1194241004b2c2f6
HarshSinghania123 <32571391+HarshSinghania123@users.noreply.github.com>
2017-10-06 18:24:11 +0530

    Update and rename sample.java to StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class StringReverse
+{
+  public static void main(String args[])
+  {
+    String s;
+    Scanner sc=new Scanner(System.in);
+    s=sc.next();
+    s.reverse();
+    System.out.println(s);
+  }
+}
